76d759aed80fd54afc1485764de324b6d46ff067
huangxin <it_huangxin@163.com>
2016-07-11 17:25:37 +0800

    修改合并AndroidManifest.xml导致的应用名为空的问题

--- library/src/main/java/com/yongchun/library/view/ImagePreviewActivity.java
+++ library/src/main/java/com/yongchun/library/view/ImagePreviewActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
